3956b1e4af51eafcb1edb8ab5e01039b5b5bc0a7
sambu-69 <BeataFerguson1371@outlook.com>
2025-12-05 08:55:49 +0800

    add IInputVerifier__factory.ts, dynamic-customer.java, report-components-notification.ts

---
+++ frontend/web/src/bridge/decorator-inventory/dynamic-customer.java
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\bridge\decorator-inventory\dynamic-customer.java
+// Generated at: 2025-10-21T15:02:43.977Z
---
+++ frontend/web/src/report-components-notification.ts
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const useresolver-validator-shared = () => { return {}; };
